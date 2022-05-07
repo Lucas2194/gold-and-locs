8deacee968e6987bd2e7753fa99fe5fc63d2a6e2
Lucas2194 <luca.okon@gmail.com>
2022-05-07 11:34:08 +0200

    Dodalem losowsc

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import numpy as np
 
-np.random.seed(5555)
+# np.random.seed(5555)
 gold = np.random.randint(low = 0, high = 10, size = (7,7))
 
-locs = np.array([
-    [0,4],
-    [2,2],
-    [2,3],
-    [5,1],
-    [6,3]
-])
+locs = np.random.randint(low = 0, high = 6, size = (5,2))
+# locs = np.array([
+#     [0,4],
+#     [2,2],
+#     [2,3],
+#     [5,1],
+#     [6,3]
+# ])
 
 for loc in locs:
     print(f' W lokalizacji {loc}, wydobyliśmy tyle złota: {gold[loc[0], loc[1]]}')
